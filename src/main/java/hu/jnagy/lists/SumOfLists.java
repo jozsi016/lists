de188af15e3b296935aca684ebf5ade3ebb2e830
Jozsef Nagy <jozsefnagy141@gmail.com>
2021-01-27 23:36:58 +0100

    Test cases all passed

--- src/main/java/hu/jnagy/lists/SumOfLists.java
+++ src/main/java/hu/jnagy/lists/SumOfLists.java
@@ -1,6 +1,8 @@
 package hu.jnagy.lists;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class SumOfLists {
     public int getSumOfList(List<Integer> list) {
@@ -8,5 +10,22 @@ public class SumOfLists {
         //return list.stream().reduce(0, (x,y) -> x+y);
         //return list.stream().reduce(0, Integer::sum);
         //return list.stream().mapToInt(Integer::intValue).sum();
+    }
+
+    public int getSumCubeOfList(List<Integer> list) {
+        return list.stream().mapToInt(i -> i*i).sum();
+    }
+
+    public int getSumOfListNegate(List<Integer> list) {
+        return list.stream().mapToInt(i -> -i).sum();
+
+    }
+
+    public int getSumMultiplyOfListElements(List<Integer> list) {
+        return list.stream().reduce(1,(x,y) -> x*y);
+    }
+
+    public int getSumOfRandomNumbers(IntStream list) {
+        return Arrays.stream(list.toArray()).sum();
     }
 }
--- src/test/java/hu/jnagy/lists/SumOfListsTest.java
+++ src/test/java/hu/jnagy/lists/SumOfListsTest.java
@@ -5,34 +5,69 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
-/*
-*   Egy rendszer ami kiszamolja egy list oszeget!
-    a negyzet minden elemre aztan oszegzes
-    lista osszes elmenek a elentes elojelenek osszeget
-    osszeselemenek a szorzata !
-    veletlen szamokat general es osszeasja!
-* */
 public class SumOfListsTest {
-    List list;
+    List<Integer> list;
     SumOfLists sumOfLists;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         sumOfLists = new SumOfLists();
     }
 
     @Test
     public void shouldSumListByAddition() {
         //Given
-        list = Arrays.asList(1,2,3,4,5);
+        list = Arrays.asList(1, 2, 3, 4, 5);
         //When
         int sum = sumOfLists.getSumOfList(list);
         //then
         assertThat(15, is(sum));
+    }
+
+    @Test
+    public void shouldSumCubeOfListElements() {
+        //Given
+        list = Arrays.asList(1, 2, 3, 4, 5);
+        //When
+        int sumOfCube = sumOfLists.getSumCubeOfList(list);
+        //then
+        assertThat(55, is(sumOfCube));
+    }
+
+    @Test
+    public void shouldSumListNegateValueByAddition() {
+        //Given
+        list = Arrays.asList(1, 2, 3, 4, 5);
+        //When
+        int sumOfNegate = sumOfLists.getSumOfListNegate(list);
+        //then
+        assertThat(-15, is(sumOfNegate));
+    }
+
+    @Test
+    public void shouldSumMultiplyOfListElements() {
+        //Given
+        list = Arrays.asList(1, 2, 3, 4, 5);
+        //When
+        int sumMultiply = sumOfLists.getSumMultiplyOfListElements(list);
+        //Then
+        assertThat(120, is(sumMultiply));
+    }
+
+    @Test
+    public void shouldSumOfRandomNumberList() {
+        //Given
+        IntStream list = new Random(1).ints(1, 5).limit(5);
+        //When
+        int sum = sumOfLists.getSumOfRandomNumbers(list);
+        //Then
+        assertTrue(5 <= sum);
     }
 }
